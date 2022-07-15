fcac6c7425d486a36024d784ca52b35379fc042d
gedelao <Gerardo_delao@ymail.com>
2022-07-15 15:31:38 -0500

    Updating submission

--- client/src/components/FormRegister.jsx
+++ client/src/components/FormRegister.jsx
@@ -1,19 +1,105 @@
 import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { useState } from 'react'
 
 function FormRegister() {
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+    state: '',
+    city: '',
+    dateOfBirth: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('/api/v1/users/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(form),
+    })
+      .then((res) => res.json())
+      .then((data) => {})
+  }
+
+  // setting each specific form to each field
+  const updateField = (name, value) => {
+    setForm({
+      ...form,
+      [name]: value,
+    })
+  }
+
   return (
     <form>
       <FormControl my="5">
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input
+          value={form.username}
+          id="username"
+          type="username"
+          onChange={(e) => updateField('username', e.target.value)}
+          required
+        />
+      </FormControl>
+      <FormControl my="5">
         <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input id="email" type="email" required />
+        <Input
+          value={form.email}
+          id="email"
+          type="email"
+          onChange={(e) => updateField('email', e.target.value)}
+          required
+        />
       </FormControl>
       <FormControl my="5">
         <FormLabel htmlFor="password">Password</FormLabel>
-        <Input id="password" type="password" required />
+        <Input
+          value={form.password}
+          id="password"
+          type="password"
+          onChange={(e) => updateField('password', e.target.value)}
+          required
+        />
       </FormControl>
-      <Button type="submit" colorScheme="orange">
-        Submit
-      </Button>
+      <div>
+        <FormControl my="5">
+          <FormLabel htmlFor="state">State</FormLabel>
+          <Input
+            value={form.state}
+            id="state"
+            type="state"
+            onChange={(e) => updateField('state', e.target.value)}
+            required
+          />
+        </FormControl>
+        <FormControl my="5">
+          <FormLabel htmlFor="city">City</FormLabel>
+          <Input
+            value={form.city}
+            id="city"
+            type="city"
+            onChange={(e) => updateField('city', e.target.value)}
+            required
+          />
+        </FormControl>
+        <FormControl my="5">
+          <FormLabel htmlFor="date">Date of Birth</FormLabel>
+          <Input
+            value={form.dateOfBirth}
+            id="date"
+            type="date"
+            onChange={(e) => updateField('dateOfBirth', e.target.value)}
+            required
+          />
+        </FormControl>
+      </div>
+
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Button type="submit" colorScheme="orange">
+          Submit
+        </Button>
+      </div>
     </form>
   )
 }
