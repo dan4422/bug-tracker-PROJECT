1c731e4ac8a513787c650966efa67db05b58ec4f
gedelao <Gerardo_delao@ymail.com>
2022-07-15 11:52:11 -0500

    updated dataBase

--- client/src/routes/Register.jsx
+++ client/src/routes/Register.jsx
@@ -1,17 +1,21 @@
 // CHAKRA:
 import { Box, Container, Text } from '@chakra-ui/react'
-import FormRegister from '../components/FormRegister'
 
 // REGISTER:
-export default function Register() {
+import React from 'react'
+import FormRegister from '../components/FormRegister'
+
+function Register() {
   return (
-    <Box border="1px" borderColor="red" w="100%" h="100%">
+    <>
       <Text px={4} mt={10} textAlign="center" fontSize="3xl">
         Register
       </Text>
       <Box my={10} mx="auto" w="95%" maxW="400px">
         <FormRegister />
       </Box>
-    </Box>
+    </>
   )
 }
+
+export default Register
