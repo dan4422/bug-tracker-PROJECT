3947859209b909185a917b97751bac64b8583fb3
Daniel Lee <sdlee095@gmail.com>
2022-07-19 11:12:03 -0400

    fixed merged code

--- client/src/routes/Projects.jsx
+++ client/src/routes/Projects.jsx
@@ -1,15 +1,12 @@
 // CHAKRA:
 import { Box, Container } from '@chakra-ui/react'
 
-import { Project } from '../components/Project'
-
 // COMPONENTS:
-import Project from '../components/Project'
+import { Project } from '../components/Project'
 
 // PROJECTS:
 export default function Projects() {
   return (
-
     <>
       <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Container style={{ boxShadow: '0px 10px 10px gray' }}>
@@ -19,10 +16,5 @@ export default function Projects() {
         </Container>
       </div>
     </>
-
-    <Box p={5} w="100%" h="100%">
-      <Project />
-    </Box>
-
   )
 }
