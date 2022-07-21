6838c8b796e21903e3cdd9d83527136384aefd95
Myles DeBoer <myles.deboer@gmail.com>
2022-07-21 11:19:24 -0700

    updated projects layout

--- client/src/components/ProjectDisplay.jsx
+++ client/src/components/ProjectDisplay.jsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Container, Flex, Wrap } from '@chakra-ui/react'
 import React from 'react'
 import { useGetProjectsQuery } from '../redux/services/projects'
 import ProjectResult from './ProjectResult'
@@ -7,9 +7,5 @@ export default function ProjectDisplay() {
   const { data, isLoading, isError } = useGetProjectsQuery()
 
   if (isLoading || isError) return null
-  return (
-    <Flex flexDirection="column">
-      {data && data.map((project) => <ProjectResult key={project.id} project={project} />)}
-    </Flex>
-  )
+  return <Wrap spacing={10}>{data && data.map((project) => <ProjectResult key={project.id} project={project} />)}</Wrap>
 }
