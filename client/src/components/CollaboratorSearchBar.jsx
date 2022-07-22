3709805c36c8ef00faecb3174d9936d44430ea4a
gedelao <Gerardo_delao@ymail.com>
2022-07-22 14:36:26 -0500

    Working on Collaboration

--- client/src/components/CollaboratorSearchBar.jsx
+++ client/src/components/CollaboratorSearchBar.jsx
@@ -1,19 +1,5 @@
-import React from 'react'
-import { PlusSquareIcon } from '@chakra-ui/icons'
-import { useSelector } from 'react-redux'
-import { Box, Button, Collapse, useDisclosure } from '@chakra-ui/react'
+import { Button, Collapse } from '@chakra-ui/react'
 
-function CollaboratorSearchBar() {
-  const { isOpen, onToggle } = useDisclosure()
-  return (
-    <>
-      <Collapse in={!isOpen} animateOpacity>
-        <Button w="100%" leftIcon={<PlusSquareIcon />} variant="outline" colorScheme="green" onClick={onToggle}>
-          Add Collaborators
-        </Button>
-      </Collapse>
-    </>
-  )
+export default function CollaboratorSearchBar() {
+  return <div>test</div>
 }
-
-export default CollaboratorSearchBar
