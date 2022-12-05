99484d08b483bb58ad8b2ccf41d9572e87dfba7a
Dan4422 <sdlee095@gmail.com>
2022-12-05 18:16:10 -0500

    update db

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'Milo072515!',
-    database: process.env.DB_DATABASE || '-',
+    database: process.env.DB_DATABASE || '',
     host: process.env.DB_HOST || 'database-bugsly.cafn11s2pstd.us-east-1.rds.amazonaws.com',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
