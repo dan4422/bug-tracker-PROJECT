20dc2317f834f1b223fad1babd8fbf31f0efae2a
Dan4422 <sdlee095@gmail.com>
2022-12-06 17:45:43 -0500

    updated config file

--- config/config.js
+++ config/config.js
@@ -5,11 +5,11 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'Milo072515!',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE || '',
-    host: process.env.DB_HOST || 'database-bugsly.cafn11s2pstd.us-east-1.rds.amazonaws.com',
-    port: process.env.DB_PORT || 5432,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   },
   production: {
