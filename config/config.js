4398ea630a21295bdff92c24630d3c765e418750
Dan4422 <sdlee095@gmail.com>
2022-12-06 15:16:13 -0500

    updated config

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE || '',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
